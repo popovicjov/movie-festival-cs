41c6d8137e93ceecd79fd5cee0e874034921091e
Jovana Cvetojevic <jovana.cvetojevic@traceone.com>
2024-10-17 11:12:50 +0200

    Add Genre class

---
+++ MovieFestival/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFestival
+{
+    public class Genre
+    {
+        public string?Name { get; set; }
+
+        public string GetData()
+        {
+            return Name[0] + " " + Name[(Name.Length) - 1];
+        }
+    }
+}
